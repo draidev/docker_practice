4c964ad94fbbb517dcf200d669aa854d79471f0e
root <root@localhost.localdomain>
2022-11-24 16:32:39 +0900

    update fortune

--- nodejs_container/hello.js
+++ nodejs_container/hello.js
@@ -3,9 +3,9 @@ const os = require('os');
 console.log("Test server starting...");
 
 var handler = function(request, response){
-	console.log("Received request from" + request.connection.remoteAddress)
+	console.log("Received request from" + request.connection.remoteAddress);
 	response.writeHead(200);
-	response.end("Container Hostname : " + os.hostname() + "\n")
+	response.end("Container Hostname : " + os.hostname() + "\n");
 
 var www = http.createServer(handler);
 www.listen(8080);
